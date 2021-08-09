9e7bf426a70072de63422ed672f0c5bc5aae14f9
Gabriel Oak <gabriel.costa41@fatec.sp.gov.br>
2021-08-09 19:02:22 -0300

    filtro por categoria

--- modules/fun/fun_module.py
+++ modules/fun/fun_module.py
@@ -6,6 +6,12 @@ from utils.intent import Intent
 from utils.module import Module
 from utils.play_audio import play_audio
 
+with open('modules/fun/jokes.json') as jokes:
+  jokes: List[dict] = list(json.load(jokes))
+
+with open('intents/general.json') as intents:
+  belongs: List[str] = list(json.load(intents)['belongs']['triggers'])
+
 class FunModule(Module):
   
   def __init__(self) -> None:
@@ -22,18 +28,33 @@ class FunModule(Module):
 
     super().__init__(intents)
 
-  def processJoke(self) -> None:
+  def processJoke(self, speech: str) -> None:
     play_audio(random.choice(self.intents['joke'].answers))
-    
-    with open('modules/fun/jokes.json') as jokes:
-      jokes: List[dict] = list(json.load(jokes))
-      
-    joke = random.choice(jokes)
+    belong = None
+    joke = None
+    for trigger in belongs:
+      if trigger in speech:
+        belong = trigger.replace(' ', '')
+        break
+
+    if type(belong) == str:
+      category = speech.split(' ')
+      i = category.index(belong) + 1
+      category = category[i]
+      print(category, 'category')
+      filteredJokes = list(filter(
+        lambda joke: category in joke['category'].lower(), 
+        jokes,
+      ))
+      joke = random.choice(filteredJokes)
+    else:
+      joke = random.choice(jokes)
+
     for phrase in joke['joke']:
       play_audio(phrase)
 
   def processCommand(self, intent: str, speech: str) -> None:
-    if (intent == 'joke'): self.processJoke()
+    if (intent == 'joke'): self.processJoke(speech=speech)
     else: play_audio(random.choice(self.intents[intent].answers))
 
     
